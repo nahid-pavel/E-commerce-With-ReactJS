100fc9d659e0ad2a732b4e92ba8e388e83e4ca83
Md. Nahid Ahmed Pavel <pavelavw@gmail.com>
2020-10-24 10:34:04 +0600

    Added about us page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { Container, Card } from "react-bootstrap";
+import "./About.style.scss";
 
 function About() {
-  return <div>About Page</div>;
+  return (
+    <div className="container mt-3 p-3">
+      <h1> About Us</h1>
+      <div className="divider"></div>
+      <p class="lead">
+        We are one of leading computer supplier and distributor for more than 10
+        years. We have special delivery system all over the country.
+      </p>
+      <div className="about_details">
+        <div className="col">
+          <div className="row">
+            <div className="col-md-4">
+              <img src="https://img.over-blog-kiwi.com/2/60/57/46/20171227/ob_2dda9d_door-to-door-courier.jpg" />
+              <div className="description">
+                <h2>Easy Delivery</h2>
+                <p className="lead">Delivery fast as possible</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <img src="https://www.seekpng.com/png/detail/269-2695182_best-quality-png-transparent-images-logo-best-quality.png" />
+              <div className="description">
+                <h2>Best Quality Products</h2>
+                <p className="lead">
+                  We always provide best quality products available in the
+                  market{" "}
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyX8hvUyGoT0C8SQdfpxAV2TyMelxqJdWgcw&usqp=CAU" />
+              <div className="description">
+                <h2>Low Price</h2>
+                <p className="lead">Great Products within budget</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
